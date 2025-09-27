4a9df7b46a3cbbcb75005406bbdfcb2f9a4c34ee
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2025-09-27 22:22:03 +0200

    Fix cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # dev uniquement
-    resource '*',
+    origins "*" # dev uniquement
+    resource "*",
       headers: :any,
       expose:  ['Authorization'],
       methods: %i[get post put patch delete options head]
